758ca007bb6dea5afed6a1415b7138f774b4e7aa
Eduardo Felipe <eduardofelipe.info@gmail.com>
2025-03-01 12:49:05 -0300

    chore: install of the biome.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="text-green-600 text-3xl font-bold bg-amber-300 ">Hello world!</div>
+      <div className="text-green-600 text-3xl font-heading ">Hello world!</div>
     </main>
   );
 }
